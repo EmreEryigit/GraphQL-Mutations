a0eeb46b4037ba46e11194ed6cd305597e587e64
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-06-11 23:13:26 +0300

    completed

---
+++ index.js
@@ -0,0 +1,225 @@
+const {ApolloServer, gql} = require('apollo-server');
+
+const {events, users,participants,locations} = require('./data');
+
+const typeDefs = gql`
+    type Event {
+        id: ID
+        title: String
+        desc: String
+        date: String
+        from: String
+        to: String
+        location: Location
+        user: User
+        participants: [Participant]
+    }
+    type Location {
+        id: ID
+        name: String
+        desc: String
+        lat: Float
+        lng: Float
+    }
+
+    type User {
+        id: ID
+        username: String
+        email: String
+        events : [Event]
+        participant: Participant
+    }
+
+    type Participant {
+        id: ID
+        user: User
+        event: Event 
+    }
+    input CreateUserInput{
+        username: String
+        email: String
+        events: String
+    }
+    input updateUser {
+        username: String
+        email: String
+    }
+    input createEventInput {
+        id: ID
+        title: String
+        desc: String
+        date: String
+        from: String
+        to: String
+        
+    }
+    input updateEvent {
+        title: String
+        desc: String
+        date: String
+        from: String
+        to: String
+    }
+    input createLocationInput {
+        id: ID
+        name: String
+        desc: String
+        lat: Float
+        lng: Float
+    }
+    input updateLocation {
+        name: String
+        desc: String
+        lat: Float
+        lng: Float
+    }
+    input createParticipantInput {
+        id: ID  
+    }
+    type deleteAllOutput {
+        count: Int
+    }
+    type Mutation {
+        #User
+        createUser(data: CreateUserInput): User
+        createEvent(data: createEventInput): Event
+        createLocation(data: createLocationInput): Location
+        createParticipant(data: createParticipantInput): Participant
+        updateUser(id: ID, data: updateUser): User
+        updateEvent(id: ID, data: updateEvent): Event
+        updateLocation(id: ID, data: updateLocation): Location
+        deleteUser(id: ID): User
+        deleteEvent(id: ID): Event
+        deleteLocation(id: ID): User
+        deleteAllUser: deleteAllOutput
+        deleteAllEvent: deleteAllOutput
+        deleteAllLocation: deleteAllOutput
+
+    }
+
+    type Query {
+        events: [Event]
+        event(id: ID): Event
+
+        users: [User]
+        user(id: ID): User
+
+        locations: [Location]
+        location(id: ID): Location!
+
+        participants: [Participant]    
+        participant(id: ID): Participant
+    }
+`
+const resolvers = {
+    Query: {
+        events: () => events,
+        event: (parent, args) => events.find(event => event.id == args.id),
+        
+        locations: () => locations,
+        location: (parent,args) => locations.find(location => location.id == args.id),
+
+        users: () => users,
+        user: (parent ,args) => users.find(user => user.id == args.id),
+
+        participants: () => participants,
+        participant: (parent,args) => participants.find(participant => participant.id == args.id)
+
+    },
+    Mutation: {
+        createUser: (parent, { data }) => {
+            
+            const newUser = {...data, id: Math.random()}
+            users.push(newUser)
+            return newUser
+        },
+        createEvent: (parent, {data}) => {
+            const newEvent = {...data, id: Math.random()}
+            events.push(newEvent)
+            return newEvent
+        },
+        createLocation: (parent, {data}) => {
+            const newLocation = {...data, id: Math.random()}
+            locations.push(newLocation)
+            return newLocation
+        },
+        createParticipant: (parent, {data}) => {
+            const newParticipant = {...data, id: Math.random()}
+            participants.push(newParticipant)
+            return newParticipant
+        },
+        updateUser: (parent, {data, id}) => {
+            const index = users.findIndex(user => user.id == id)
+            users[index] = {...users[index], ...data}
+            return users[index]
+            
+        },
+        updateEvent: (parent, {data, id}) => {
+            const index = events.findIndex(event => event.id == id)
+            events[index] = {...events[index], ...data}
+            return events[index]
+            
+        },
+        updateLocation: (parent, {data, id}) => {
+            const index = locations.findIndex(location => location.id == id)
+            locations[index] = {...locations[index], ...data}
+            return locations[index]
+            
+        },
+        deleteUser: (parent, {id}) => {
+            const index = users.findIndex(user => user.id == id)
+            users.splice(index, 1)
+            return users[index]
+        },
+        deleteEvent: (parent, {id}) => {
+            const index = events.findIndex(event => event.id == id)
+            events.splice(index, 1)
+            return events[index]
+        },
+        deleteLocation: (parent, {id}) => {
+            const index = locations.findIndex(location => location.id == id)
+            locations.splice(index, 1)
+            return locations[index]
+        },
+        deleteAllUser: () => {
+            const length = users.length
+            users.splice(0,length)
+            return {
+                count: length
+            }
+        },
+        deleteAllEvent: () => {
+            const length = events.length
+            events.splice(0,length)
+            return {
+                count: length
+            }
+        },
+        deleteAllLocation: () => {
+            const length = locations.length
+            locations.splice(0,length)
+            return {
+                count: length
+            }
+        }
+
+    },
+    User: {
+        events: (parent) => events.filter(event => event.user_id == parent.id),
+        participant: (parent) => participants.find(p => p.user_id == parent.id),
+        
+    },
+    Event: {
+        location: (parent) => locations.find(location => location.id == parent.location_id),
+        participants: (parent) => participants.filter(p => p.event_id == parent.id),
+        user: (parent) => users.find(user => user.id == parent.user_id)
+    },
+    Participant: {
+        user: (parent) => users.find(user => user.id == parent.user_id),
+        event: (parent) => events.find(event => event.id == parent.event_id)
+    }
+   
+}
+const server = new ApolloServer({typeDefs, resolvers});
+
+server .listen().then(({url}) => console.log('Server is running on ' + url));
